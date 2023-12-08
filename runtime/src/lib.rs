0348a6dab329e2a053e39f8ab37d7b2f069197e0
driemworks <tonyrriemer@gmail.com>
2023-12-08 14:58:46 -0600

    adjust storage limits for contract

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -326,9 +326,9 @@ impl frame_support::traits::Contains<RuntimeCall> for AllowBalancesCall {
 }
 
 parameter_types! {
-	pub const DepositPerItem: Balance = deposit(1, 0);
-	pub const DepositPerByte: Balance = deposit(0, 1);
-	pub const DefaultDepositLimit: Balance = deposit(1024, 1024 * 1024);
+	pub const DepositPerItem: Balance = deposit(0, 1);
+	pub const DepositPerByte: Balance = deposit(1, 0);
+	pub const DefaultDepositLimit: Balance = deposit(128 * 1024, 2 * 1024 * 1024);
 	pub Schedule: pallet_contracts::Schedule<Runtime> = Default::default();
 	pub CodeHashLockupDepositPercent: Perbill = Perbill::from_percent(30);
 }
