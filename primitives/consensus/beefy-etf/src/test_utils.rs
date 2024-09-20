47eff7c36775c43674cbbadb25a8daef7c182d49
driemworks <driemworks@idealabs.network>
2024-09-20 15:13:57 -0500

    feat: add test functions, formatting

--- primitives/consensus/beefy-etf/src/test_utils.rs
+++ primitives/consensus/beefy-etf/src/test_utils.rs
@@ -15,19 +15,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// #[cfg(feature = "bls-experimental")]
-// use crate::bls_bls_crypto;
 use crate::{
 	bls_crypto, AuthorityIdBound, BeefySignatureHasher, Commitment, EquivocationProof, Payload,
-	ValidatorSetId, VoteMessage,
+	ValidatorSetId, VoteMessage, bls_crypto::{AuthorityId as BeefyId},
 };
-use sp_application_crypto::{AppCrypto, AppPair, RuntimeAppPublic, Wraps};
+use sp_application_crypto::{AppCrypto, AppPair, RuntimeAppPublic, Wraps, UncheckedFrom};
 use sp_core::{bls377, Pair};
 use sp_runtime::traits::Hash;
 
 use codec::Encode;
 use std::{collections::HashMap, marker::PhantomData};
 use strum::IntoEnumIterator;
+
+
+use ark_serialize::CanonicalSerialize;
+use ark_std::UniformRand;
+use etf_crypto_primitives::{
+	proofs::hashed_el_gamal_sigma::BatchPoK,
+	dpss::acss::DoubleSecret
+};
+use rand::rngs::OsRng;
+use w3f_bls::{DoublePublicKey, DoublePublicKeyScheme, EngineBLS, SerializableToBytes, TinyBLS377};
 
 /// Set of test accounts using [`crate::bls_crypto`] types.
 #[allow(missing_docs)]
@@ -52,18 +60,6 @@ pub trait BeefySignerAuthority<MsgHash: Hash>: AppPair {
 	fn sign_with_hasher(&self, message: &[u8]) -> <Self as AppCrypto>::Signature;
 }
 
-// impl<MsgHash> BeefySignerAuthority<MsgHash> for <bls_crypto::AuthorityId as AppCrypto>::Pair
-// where
-// 	MsgHash: Hash,
-// 	<MsgHash as Hash>::Output: Into<[u8; 32]>,
-// {
-// 	fn sign_with_hasher(&self, message: &[u8]) -> <Self as AppCrypto>::Signature {
-// 		let hashed_message = <MsgHash as Hash>::hash(message).into();
-// 		self.as_inner_ref().sign_prehashed(&hashed_message).into()
-// 	}
-// }
-
-// #[cfg(feature = "bls-experimental")]
 impl<MsgHash> BeefySignerAuthority<MsgHash> for <bls_crypto::AuthorityId as AppCrypto>::Pair
 where
 	MsgHash: Hash,
@@ -137,25 +133,6 @@ impl From<Keyring<bls_crypto::AuthorityId>> for bls_crypto::Public {
 }
 
 /// Create a new `EquivocationProof` based on given arguments.
-pub fn test_generate_equivocation_proof<BN>(
-	vote1: (BN, Payload, ValidatorSetId, &Keyring<bls_crypto::AuthorityId>),
-	vote2: (BN, Payload, ValidatorSetId, &Keyring<bls_crypto::AuthorityId>),
-) -> EquivocationProof<BN, bls_crypto::Public, bls_crypto::Signature> {
-	let signed_vote = |block_number: BN,
-	                   payload: Payload,
-	                   validator_set_id: ValidatorSetId,
-	                   keyring: &Keyring<bls_crypto::AuthorityId>| {
-		let commitment = Commitment { validator_set_id, block_number, payload };
-		let signature = keyring.sign(&b"idc".to_vec());
-		VoteMessage { commitment, id: keyring.public(), signature }
-	};
-	let first = signed_vote(vote1.0, vote1.1, vote1.2, vote1.3);
-	let second = signed_vote(vote2.0, vote2.1, vote2.2, vote2.3);
-	EquivocationProof { first, second }
-}
-
-
-/// Create a new `EquivocationProof` based on given arguments.
 pub fn generate_equivocation_proof(
 	vote1: (u64, Payload, ValidatorSetId, &Keyring<bls_crypto::AuthorityId>),
 	vote2: (u64, Payload, ValidatorSetId, &Keyring<bls_crypto::AuthorityId>),
@@ -171,4 +148,63 @@ pub fn generate_equivocation_proof(
 	let first = signed_vote(vote1.0, vote1.1, vote1.2, vote1.3);
 	let second = signed_vote(vote2.0, vote2.1, vote2.2, vote2.3);
 	EquivocationProof { first, second }
+}
+
+/// Helper function to prepare initial secrets and resharing for ETF conensus
+/// return a vec of (authority id, resharing, pubkey commitment) along with ibe public key against the master secret
+pub fn etf_genesis<E: EngineBLS>(
+    initial_authorities: Vec<BeefyId>,
+) -> (Vec<u8>, Vec<(BeefyId, Vec<u8>)>) {
+    let msk_prime = E::Scalar::rand(&mut OsRng);
+    let keypair = w3f_bls::KeypairVT::<E>::generate(&mut OsRng);
+    let msk: E::Scalar = keypair.secret.0;
+    let double_public: DoublePublicKey<E> = DoublePublicKey(
+        keypair.into_public_key_in_signature_group().0,
+        keypair.public.0,
+    );
+
+    let double_secret = DoubleSecret::<E>(msk, msk_prime);
+
+    let mut double_public_bytes = Vec::new();
+    double_public
+        .serialize_compressed(&mut double_public_bytes)
+        .unwrap();
+
+    let genesis_resharing: Vec<(
+		DoublePublicKey<E>,
+		BatchPoK<E::PublicKeyGroup>
+	)> = double_secret
+        .reshare(
+            &initial_authorities
+                .iter()
+                .map(|authority| {
+                    w3f_bls::single::PublicKey::<E>(
+                        w3f_bls::double::DoublePublicKey::<E>::from_bytes(&authority.to_raw_vec())
+                            .unwrap()
+                            .1,
+                    )
+                })
+                .collect::<Vec<_>>(),
+            initial_authorities.len() as u8, // threshold = full set of authorities for now
+            &mut OsRng,
+        ).unwrap();
+
+	let serialized_resharings = initial_authorities
+        .iter()
+        .enumerate()
+        .map(|(idx, _)| {
+			let pk = &genesis_resharing[idx].0;
+			let mut pk_bytes = [0u8;144];
+			pk.serialize_compressed(&mut pk_bytes[..]).unwrap();
+			let blspk = bls377::Public::unchecked_from(pk_bytes);
+
+            let pok = &genesis_resharing[idx].1;
+            let mut bytes = Vec::new();
+			pok.serialize_compressed(&mut bytes).unwrap();
+			
+			let beefy_id = BeefyId::from(blspk);
+			(beefy_id, bytes)
+        })
+        .collect::<Vec<_>>();
+    (double_public_bytes, serialized_resharings)
 }
